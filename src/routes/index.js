ba2f2b4a0e44895312e6c29f53221193cbacf407
Michael Washburn <m.washburn@mail.com>
2022-04-21 08:31:16 -0700

    configured eslint, prettier and babel

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'SDC IS FUN!!!' });
-});
+const express = require('express');
+
+const router = express.Router();
+router.get('/', (req, res, next) => res.status(200).json({ message: 'SDC IS FUN!!!' }));
 module.exports = router;
